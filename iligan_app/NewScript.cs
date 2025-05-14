d072924c153c047b1625ce5b52a597d15e2fd83e
syko.dev <87885079+sykosanity@users.noreply.github.com>
2025-05-14 16:28:26 +0800

    Add files via upload

---
+++ iligan_app/NewScript.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class NewScript : Node
+{
+	
+	public override void _Ready() {
+		GD.Print("Hello World!");
+
+	}
+	
+	public override void _Process(double data) {
+		
+	}
+	
+}
---
+++ iligan_app/Node3d.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Node3d : Node2D
+{
+	
+	public override void _Ready() {
+		GD.Print("HELLOOOO");
+	}
+	
+	public override void _Process(double data) {
+		
+	}
+	
+}
