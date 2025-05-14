9d086db2101969b8b145b037e8efb7dd898003d6
syko.dev <87885079+sykosanity@users.noreply.github.com>
2025-05-14 16:36:46 +0800

    Add files via upload

--- iligan_app/NewScript.cs
+++ iligan_app/NewScript.cs
@@ -3,14 +3,29 @@ using System;
 
 public partial class NewScript : Node
 {
-	
-	public override void _Ready() {
-		GD.Print("Hello World!");
 
+	public override void _Ready()
+	{
+		int[,] multTable = new int[11, 11];
+
+		// Fill the multiplication table
+		for (int x = 1; x < 11; x++)
+		{
+			for (int y = 0; y < 11; y++)
+			{
+				multTable[y, x] = y * x;
+			}
+		}
+
+		// Print the multiplication table
+		for (int y = 1; y < 11; y++)
+		{
+			string line = "";
+			for (int x = 1; x < 11; x++)
+			{
+				line += $"{multTable[y, x],4}";
+			}
+			GD.Print(line);
+		}
 	}
-	
-	public override void _Process(double data) {
-		
-	}
-	
 }
